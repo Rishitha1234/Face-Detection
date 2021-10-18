d963b4417fd19c66e75e224ec0bab670ba64ed0e
Rishitha1234 <64676249+Rishitha1234@users.noreply.github.com>
2021-10-18 16:58:21 +0530

    Create detection.py

---
+++ detection.py
@@ -0,0 +1,13 @@
+import cv2
+
+model= cv2.CascadeClassifier(r"C:\Users\Rishi\Downloads\haarcascade_frontalface_default.xml")
+img=cv2.imread(r"C:\Users\Rishi\Downloads\cv.jpg")
+imgGray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces= model.detectMultiScale(imgGray,1.1,4)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    
+cv2.imshow("result",img)
+cv2.waitKey(0)
